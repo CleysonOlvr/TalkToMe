cc3f9a95d6b32849cf66fcae92eff5d12ba737e5
Cleyson Oliveira <cleyson.gc@gmail.com>
2024-02-07 00:05:09 -0300

    feat: added initial P2P RTC connection config

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -8,6 +8,7 @@ import { useContext, useEffect, useRef } from "react";
 export default function Room({ params }: { params: { id: string } }) {
   const { socket } = useContext(SocketContext);
   const localStream = useRef<HTMLVideoElement>(null);
+  const peerConnections = useRef<Record<string, RTCPeerConnection>>({});
 
   useEffect(() => {
     socket?.on("connection", async () => {
@@ -18,7 +19,49 @@ export default function Room({ params }: { params: { id: string } }) {
       });
       await initCamera();
     });
+
+    socket?.on("newUserStart", (data) => {
+      console.log("Novo usuário conectado", data);
+      createPeerConnection(data.sender, true);
+    });
+
+    socket?.on("new user", (data) => {
+      console.log("Novo usuário conectado", data);
+      createPeerConnection(data.socketId, false);
+      socket.emit("newUserStart", { to: data.socketId, sender: socket.id });
+    });
+    socket?.on("sdp", async (data) => {
+      console.log("SDP", data);
+    });
   }, [socket]);
+
+  const createPeerConnection = async (
+    socketId: string,
+    createOffer: boolean
+  ) => {
+    const config = {
+      iceServers: [
+        {
+          urls: "stun:stun.l.google.com:19302",
+        },
+      ],
+    };
+
+    const peer = new RTCPeerConnection(config);
+    peerConnections.current[socketId] = peer;
+
+    if (createOffer) {
+      const peerConnection = peerConnections.current[socketId];
+
+      const offer = await peerConnection.createOffer();
+      await peerConnection.setLocalDescription(offer);
+
+      socket?.emit("sdp", {
+        to: socketId,
+        description: peerConnection.localDescription,
+      });
+    }
+  };
 
   const initCamera = async () => {
     const video = await navigator.mediaDevices.getUserMedia({
@@ -39,7 +82,7 @@ export default function Room({ params }: { params: { id: string } }) {
           <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
             <div className="bg-gray-950 w-full h-full rounded-md p-2 relative">
               <video
-                className="h-full w-full"
+                className="h-full w-full mirror-mode"
                 autoPlay
                 ref={localStream}
               ></video>
